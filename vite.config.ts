f03c1707f1104eda59b736da88e8561d8982f194
Rindut <damastaofrina@gmail.com>
2025-03-27 14:01:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/",
+  base: "/fur-friend-folio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
